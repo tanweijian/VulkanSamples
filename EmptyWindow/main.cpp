1b3de71146eaba743230811ca2c38e98faad81ea
tanweijian <tanweijian199412@gmail.com>
2021-09-28 16:43:52 +0800

    change project settings

--- EmptyWindow/main.cpp
+++ EmptyWindow/main.cpp
@@ -1,28 +1,4 @@
-/*
- * Vulkan Windowed Program
- *
- * Copyright (C) 2016, 2018 Valve Corporation
- * Copyright (C) 2016, 2018 LunarG, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
- /*
- Vulkan C++ Windowed Project Template
- Create and destroy a Vulkan surface on an SDL window.
- */
-
- // Enable the WSI extensions
+// Enable the WSI extensions
 #if defined(__ANDROID__)
 #define VK_USE_PLATFORM_ANDROID_KHR
 #elif defined(__linux__)
